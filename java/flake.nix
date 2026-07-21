cc772d403e06fa0f39b736b57870db1209bf6aa5
Noodl3r <echelon.r6s@gmail.com>
2026-07-20 21:53:14 -0400

    Adding a java flake

---
+++ java/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix dev shell for Java";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "c";
+        packages = with pkgs; [
+          jdk21
+          jdt-language-server
+          gradle
+          maven
+        ];
+      };
+    });
+}
